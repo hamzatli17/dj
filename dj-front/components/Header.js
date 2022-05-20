56740b14fd1f6514990cd8bc4dff8279023bc752
hamza tlili <hamzatli787@gmail.com>
2022-05-20 13:56:16 +0100

    search component

--- dj-front/components/Header.js
+++ dj-front/components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/Link'
 import styles from '@/styles/Header.module.css'
-
+import Search from './Search'
 export default function Header() {
   return (
     <header className={styles.header}>
@@ -9,6 +9,7 @@ export default function Header() {
                <a>DJ Events</a>
            </Link>
       </div>
+      <Search />
       <nav>
           <ul>
               <li>
