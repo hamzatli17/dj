1f2f8cd2b7135aefff5984324ee51795e148dfaf
hamza tlili <hamzatli787@gmail.com>
2022-06-08 16:00:16 +0100

    login consommation Api

--- dj-front/pages/account/register.js
+++ dj-front/pages/account/register.js
@@ -2,21 +2,21 @@
 import { FaUser } from 'react-icons/fa'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useContext } from 'react'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
-//import AuthContext from '@/context/AuthContext'
+import AuthContext from '@/context/AuthContext'
 import styles from '@/styles/AuthForm.module.css'
-
+//import AuthContext from '@/context/AuthContext'
 export default function RegisterPage() {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
-  //const { register, error } = useContext(AuthContext)
+  const { register, error } = useContext(AuthContext)
 
- // useEffect(() => error && toast.error(error))
+  useEffect(() => error && toast.error(error))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,6 +27,7 @@ export default function RegisterPage() {
     }
 
    console.log({ username, email, password })
+   register({ username, email, password })
   }
 
   return (
